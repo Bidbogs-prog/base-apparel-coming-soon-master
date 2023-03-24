324c3178e2144e767ae66a0dfa314338dfcaddbb
Bidbogs-prog <haythamchhilif@gmail.com>
2023-03-24 20:11:16 +0000

    added positive feedback

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const submit = document.querySelector('.btn');
 const inputText = document.querySelector('input')
 const feedback = document.querySelector('.feedback')
 const error = document.querySelector('.error')
+const feedbackP = document.querySelector('.feedbackP')
 const pattern = /[-A-Za-z0-9!#$%&'*+\/=?^_`{|}~]+(?:\.[-A-Za-z0-9!#$%&'*+\/=?^_`{|}~]+)*@(?:[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?\.)+[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?/i;
 const emailPattern = pattern
 
@@ -12,10 +13,11 @@ submit.addEventListener('click', e =>{
     if (emailPattern.test(email)){
         feedback.style.display = 'none'
         error.style.display = 'none'
+        feedbackP.style.display = 'block'
     } else{
         feedback.style.display = 'block'
         error.style.display = 'block'
-
+        feedbackP.style.display = 'none'
     }
 
 }
