6583ca0fadbfb3ed03e97671b82d07ec2a8a927d
Bidbogs-prog <haythamchhilif@gmail.com>
2023-03-24 18:36:06 +0000

    added js functionality

---
+++ index.js
@@ -0,0 +1,21 @@
+const submit = document.querySelector('.btn');
+const inputText = document.querySelector('input')
+const feedback = document.querySelector('.feedback')
+const error = document.querySelector('.error')
+const pattern = /[-A-Za-z0-9!#$%&'*+\/=?^_`{|}~]+(?:\.[-A-Za-z0-9!#$%&'*+\/=?^_`{|}~]+)*@(?:[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?\.)+[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?/i;
+const emailPattern = pattern
+
+submit.addEventListener('click', e =>{
+    e.preventDefault();
+    const email = inputText.value
+
+    if (emailPattern.test(email)){
+        console.log("success")
+    } else{
+        feedback.style.display = 'block'
+        error.style.display = 'block'
+
+    }
+
+}
+)
